8d1ba2671e5c35edbf76de0994b26ad9d78c5a39
Lukasz Rydz <lukasz.rydz@outlook.com>
2025-01-04 20:27:32 +0100

    NGROK address changed

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,17 +1,19 @@
 // export class Host {
-//     public static readonly SHOP = 'http://localhost:8050';
-//     public static readonly CLIENT = 'http://localhost:8080';
-//     public static readonly ADMIN = 'http://localhost:8000';
-//     public static readonly BOT = 'http://localhost:5010';
-//     public static readonly SPOTIFY = 'http://localhost:5000';
-//     public static readonly FRONT = 'http://localhost:5173';
+//     public static readonly SHOP = `http://localhost:8050`;
+//     public static readonly CLIENT = `http://localhost:8080`;
+//     public static readonly ADMIN = `http://localhost:8000`;
+//     public static readonly BOT = `http://localhost:5010`;
+//     public static readonly SPOTIFY = `http://localhost:5000`;
+//     public static readonly FRONT = `http://localhost:5173`;
 // }
 
 export class Host {
-    public static readonly SHOP = 'http://localhost:5000/shop';
-    public static readonly CLIENT = 'http://localhost:5000/client'; 
-    public static readonly ADMIN = 'http://localhost:5000/admin';    
-    public static readonly BOT = 'http://localhost:5000/bot';        
-    public static readonly SPOTIFY = 'http://localhost:5000/spotify';
-    public static readonly FRONT = 'http://localhost:5173';         
+    private static readonly NGROK_HOST = `https://ef06-185-87-137-39.ngrok-free.app`;
+
+    public static readonly SHOP = `${this.NGROK_HOST}/shop`;
+    public static readonly CLIENT = `${this.NGROK_HOST}/client`; 
+    public static readonly ADMIN = `${this.NGROK_HOST}/admin`;    
+    public static readonly BOT = `${this.NGROK_HOST}/bot`;        
+    public static readonly SPOTIFY = `${this.NGROK_HOST}/spotify`;
+    public static readonly FRONT = `http://localhost:5173`;         
 }
